46bf3cefa9ee9e4d08bc20a682ff16ea0fd24602
Tristan Jones <tristankylerjones@gmail.com>
2021-12-11 13:24:40 -0600

    click task working

--- click/app.js
+++ click/app.js
@@ -1,1 +1,6 @@
 // make the box disapear when the user clicks it
+
+document.getElementsByClassName("red-box")[0].onclick = function()
+{
+    document.getElementsByClassName("red-box")[0].style.display="none"; 
+}
