ed2b628980b56d02b5231b3c2960e98432c18881
Tristan Jones <tristankylerjones@gmail.com>
2021-12-11 19:40:09 -0600

    keypress task working

--- keypress/app.js
+++ keypress/app.js
@@ -3,3 +3,8 @@
 Example of output: You've pressed the "a" key. It's key code is 65.
 
 Wes Bos made this https://keycode.info/ for us to use as an example. */
+
+document.onkeydown = function(event) {
+    let pressedKey = event.key || '';
+    document.getElementById('output').textContent ='Key Pressed: ' + pressedKey + ' ----- Key Code: ' + event.which || '';
+};
