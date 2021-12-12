c83bc59feb7afaa8f2c573bb7bd8c9ad34206ce2
Tristan Jones <tristankylerjones@gmail.com>
2021-12-11 19:55:34 -0600

    mousemove task done

---
+++ mousemove/app.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("red-box")[0].onmousemove = function(event)
+{
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "Coordinates: X: " + x + ", Y: " + y;
+    document.getElementsByClassName("red-box")[0].textContent = coords;
+}
